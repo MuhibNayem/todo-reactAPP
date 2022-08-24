1f7dc7b38b7b0a54bda538574dcdea940ddab235
lucifer7663 <a.k.m.muhibullah.nayem@g.bracu.ac.bd>
2022-08-24 13:01:10 +0600

    todo app done

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -4,8 +4,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
 import ListItemText from "@mui/material/ListItemText";
 
-const TodoList = ({todos, setTodos}) => {
-
+const  SingleListItem = ({todo,setTodos,todos}) => {
     let [isHovering, setIsHovering] = useState(false)
 
     function handleMouseEntrance(e)  {
@@ -31,36 +30,43 @@ const TodoList = ({todos, setTodos}) => {
         setTodos(todos.filter((todo) => todo.id !== id))
     }
 
+    return(
+        <ListItem
+            key={todo.id}
+            onMouseOver={handleMouseEntrance}
+            onMouseOut = {handleMouseLeave}
+            className=
+            {`list-item ${ isHovering ? "list-item-focus" : ""}`}
+            
+        >
+            <ListItemText
+                primary = {todo.title}
+                className={`list ${todo.completed ? "complete" : ""}`}
+            />
+            <ListItemSecondaryAction
+                onMouseOver={handleMouseEntrance}
+                onMouseOut = {handleMouseLeave} 
+                className= {`${ isHovering ? "" : "listItemSecondaryAction-hidden"}`}
+            >
+                <button className="button-complete" onClick={() => handleDone(todo)}>
+                    <i className="fa-solid fa-circle-check"></i>
+                </button>
+                <button className="button-delete" onClick={() => handleDelete(todo)}>
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
+}
+
+const TodoList = ({todos, setTodos}) => {
+
     return (
         <div>        
             <List>
-                {todos.map((todo) => (
-                    <ListItem
-                        key={todo.id}
-                        onMouseOver={handleMouseEntrance}
-                        onMouseOut = {handleMouseLeave}
-                        className=
-                        {`list-item ${ isHovering ? "list-item-focus" : ""}`}
-                    >
-                        <ListItemText
-                            primary = {todo.title}
-                            className={`list ${todo.completed ? "complete" : ""}`}
-                        />
-                        <ListItemSecondaryAction
-                            onMouseOver={handleMouseEntrance}
-                            onMouseOut = {handleMouseLeave} 
-                            className= {`${ isHovering ? "" : "listItemSecondaryAction-hidden"}`}
-                        >
-                            <button className="button-complete" onClick={() => handleDone(todo)}>
-                                <i className="fa-solid fa-circle-check"></i>
-                            </button>
-                            <button className="button-delete" onClick={() => handleDelete(todo)}>
-                                <i className="fa-solid fa-trash"></i>
-                            </button>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                               
-                ))}
+                {todos.map(todo => {
+                    return <SingleListItem todo = {todo} setTodos={setTodos} todos = {todos}/>;
+                })}
             </List> 
         </div>
     );
